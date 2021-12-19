7f5d16e3767d3d686864e834323c853953ebda50
Christopher Smith <christopher@sixbyte.com>
2021-12-19 16:22:09 -0600

    Update TurretTestCTSDELETE.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TurretTestCTSDELETE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TurretTestCTSDELETE.java
@@ -141,7 +141,7 @@ public class TurretTestCTSDELETE extends LinearOpMode {
         }
 
         // nested while loops are used to allow for a final check of an overshoot situation
-        while ((Math.abs(error) > 0) && opModeIsActive()) {
+        while ((Math.abs(error) > 5) && opModeIsActive()) {
             derivative = lastError - error;
             rotationSpeed = ((Cp * error) + (Ci * integral) + (Cd * derivative)) * maxSpeed;
 
