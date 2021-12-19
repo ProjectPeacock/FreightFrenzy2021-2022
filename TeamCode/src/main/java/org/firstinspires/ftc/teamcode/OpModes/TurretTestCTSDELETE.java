6211157cf7db28c81ed6093517eeddcbc8f15974
Christopher Smith <christopher@sixbyte.com>
2021-12-19 15:00:29 -0600

    Update TurretTestCTSDELETE.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TurretTestCTSDELETE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TurretTestCTSDELETE.java
@@ -85,8 +85,9 @@ public class TurretTestCTSDELETE extends LinearOpMode {
             }
 
             if(gamepad2.right_stick_x != 0){
-                turretControl.setTurretRotation(gamepad2.right_stick_x);
-            }
+//                turretControl.setTurretRotation(gamepad2.right_stick_x);
+                robot.turretServoBlue.setPower(gamepad2.right_stick_x);
+                robot.turretServoPink.setPower(gamepad2.right_stick_x);            }
             // apply the changes to the turret
             turretControl.setTargetPosition(turretPosition);
 
