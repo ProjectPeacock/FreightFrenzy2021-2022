76763dcedb033552ebfff0d87c45265d503d0aab
jgriffin0622 <41241607+jgriffin0622@users.noreply.github.com>
2021-12-21 13:42:16 -0600

    arm control class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/ArmControlCLass.java
@@ -0,0 +1,219 @@
+// change comments
+
+/*
+  12/09/21 added rotateTurret & resetTurret method stubs
+ */
+package org.firstinspires.ftc.teamcode.Libs;
+
+import static java.lang.Thread.sleep;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.HardwareProfile.HardwareProfile;
+
+public class ArmControlCLass {
+    //takes in HardwareProfile to be able to access motors & servos
+    public HardwareProfile localRobot=null;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private int angle1=0;
+    private int angle2=0;
+    private double arm1Power=1;
+    private double arm2Power=1;
+    private int sleepTime;
+    private boolean isRunning=true;
+    private double turretPower = 0.02;
+    private int turretTargetPosition = 0;
+    private int turretThreshold = 5;
+    private int turretLimit = 330;
+    private double timeElapsed;
+    private int turretServoPower=0;
+
+    //constructor
+    public ArmControlCLass(HardwareProfile robotIn, int threadSleepDelay){
+        this.localRobot=robotIn;
+        this.sleepTime=threadSleepDelay;
+    }
+
+//deploy intake method
+    public void intakeOn(boolean deployed){
+        localRobot.intakeDeployBlue.setPosition(localRobot.BLUE_ZERO - localRobot.INTAKE_DEPLOY_BLUE);
+        localRobot.intakeDeployPink.setPosition(localRobot.PINK_ZERO + localRobot.INTAKE_DEPLOY_PINK);
+        localRobot.intakeTilt.setPosition(localRobot.INTAKE_TILT_INPUT);
+        angle2=localRobot.ARM_2_INTAKE;
+        localRobot.motorArmAngle2.setTargetPosition(angle2);
+        while(localRobot.motorArmAngle2.getCurrentPosition()>1150){
+
+        }
+        angle1=localRobot.ARM_1_INTAKE;
+        if(localRobot.motorArmAngle1.getCurrentPosition()>900) {
+            localRobot.motorIntake.setPower(localRobot.INTAKE_POW);
+        }
+    }
+//end of deploy intake method
+
+//retract intake method
+    public void intakeOff(boolean deployed){
+        if(!deployed){
+            angle1=0;
+            angle2=0;
+        }
+        //waits for arm 1 to move up before moving intake to prevent collisions
+        if(localRobot.motorArmAngle1.getCurrentPosition()<750){
+            localRobot.motorIntake.setPower(0);
+            localRobot.intakeDeployBlue.setPosition(localRobot.BLUE_ZERO);
+            localRobot.intakeDeployPink.setPosition(localRobot.PINK_ZERO);
+            localRobot.intakeTilt.setPosition(localRobot.INTAKE_STARTING_POS);
+        }
+    }
+//end of retract intake method
+
+//reset intake without moving arm (for retracting intake while scoring)
+    public void resetIntake(){
+        if(localRobot.motorArmAngle1.getCurrentPosition()<750){
+            localRobot.motorIntake.setPower(0);
+            localRobot.intakeDeployBlue.setPosition(localRobot.BLUE_ZERO);
+            localRobot.intakeDeployPink.setPosition(localRobot.PINK_ZERO);
+            localRobot.intakeTilt.setPosition(localRobot.INTAKE_STARTING_POS);
+        }
+    }
+//end of rest intake without moving arm
+
+//move to scoring positions methods
+    //high platform scoring (default)
+    public void scoringPos1(){
+        angle1=-350;
+        if(localRobot.motorArmAngle1.getCurrentPosition()<750){
+            angle2=localRobot.HIGH_PLATFORM;
+            setArmPosition();
+        }
+    }
+    //mid platform scoring
+    public void scoringPos2(){
+        angle1=-350;
+        angle2=localRobot.MID_PLATFORM;
+        setArmPosition();
+    }
+    //low platform & shared shipping hub scoring
+    public void scoringPos3(){
+        angle1=-1000;
+        angle2=localRobot.LOW_PLATFORM;
+        setArmPosition();
+    }
+//end of scoring positions methods
+
+//hard arm reset method (DO NOT USE IF POSSIBLE)
+    public void resetArm(){
+        angle1=0;
+        angle2=2786; //moves arm 2 to one full rotation
+        while(localRobot.motorArmAngle2.getCurrentPosition()!=angle2){
+
+        }
+        localRobot.motorArmAngle2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        localRobot.motorArmAngle2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+//end of hard arm reset method
+
+//soft arm reset method (returns to original zero)
+    //moves arm to 0 (over the top)
+    public void moveToZero(){
+        angle1=0;
+        angle2=0;
+    }
+//end of soft arm reset method
+//arm 2 position manual increment methods
+    public void incrementUp(){
+        angle2+=5;
+    }
+    public void incrementDown(){
+        angle2-=5;
+    }
+//end of arm 2 manual increment methods
+
+//manually set power for arm motors
+    public void setPower(double a, double b){
+        arm1Power=a;
+        arm2Power=b;
+    }
+//end of manually set power for arm motors
+
+    //return motorArmAngle1 position for auto
+    public int getEncoderAngle1(){
+        return localRobot.motorArmAngle1.getCurrentPosition();
+    }
+
+    //return motorArmAngle2 position for auto
+    public int getEncoderAngle2(){
+        return localRobot.motorArmAngle2.getCurrentPosition();
+    }
+
+//set custom position for arm motors
+    public void setAutoPosition(int a, int b){
+        localRobot.motorArmAngle1.setTargetPosition(a);
+        localRobot.motorArmAngle2.setTargetPosition(b);
+    }
+//end of set custom position for arm motors
+
+//chainsaw acceleration for Blue Alliance
+    public void chainsawRampBlue(){
+        for(int i=localRobot.CHAIN_INCREMENTS; i>0;i--){
+            localRobot.motorChainsaw.setPower(-localRobot.CHAIN_POW/i);
+        }
+    }
+//end of chainsaw acceleration for Blue Alliance
+
+//chainsaw acceleration for Red Alliance
+    public void chainsawRampRed(){
+        for(int i=localRobot.CHAIN_INCREMENTS; i>0;i--){
+            localRobot.motorChainsaw.setPower(localRobot.CHAIN_POW/i);
+        }
+    }
+//end of chainsaw acceleration for Red Alliance
+
+// rotate turret
+    public void rotateTurret(double power){
+        // ensure turret within limit
+        if (Math.abs(localRobot.turrentEncoder.getCurrentPosition())<turretLimit){
+            localRobot.turretServoBlue.setPower(power);
+            localRobot.turretServoPink.setPower(power);
+        }
+    }   // end of rotateTurret
+
+    // reset turret - returns turret to 'home' position
+    public void resetTurret(){
+        while(Math.abs(localRobot.turrentEncoder.getCurrentPosition())>turretTargetPosition) {
+            if(Math.abs(localRobot.turrentEncoder.getCurrentPosition())>50){
+                if (localRobot.turrentEncoder.getCurrentPosition() > turretTargetPosition) {
+                    turretPower = 0.3;
+                } else {
+                    turretPower = -0.3;
+                }
+            }else if(Math.abs(localRobot.turrentEncoder.getCurrentPosition())<=50){
+                if (localRobot.turrentEncoder.getCurrentPosition() > turretTargetPosition) {
+                    turretPower = 0.1;
+                } else {
+                    turretPower = -0.1;
+                }
+            }else if(Math.abs(localRobot.turrentEncoder.getCurrentPosition())<20){
+                if (localRobot.turrentEncoder.getCurrentPosition() > turretTargetPosition) {
+                    turretPower = 0.005;
+                } else {
+                    turretPower = -0.005;
+                }
+            }
+            localRobot.turretServoBlue.setPower(turretPower);
+            localRobot.turretServoPink.setPower(turretPower);
+        }
+    }   // end of resetTurret
+
+//method that runs whenever thread is running
+    public void setArmPosition(){
+        localRobot.motorArmAngle1.setPower(arm1Power);
+        localRobot.motorArmAngle2.setPower(arm2Power);
+        localRobot.motorArmAngle1.setTargetPosition(angle1);
+        localRobot.motorArmAngle2.setTargetPosition(angle2);
+    } // end setArmPosition
+
+}
+//end of thread run method
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AutoParkDriveClassJG2.java
@@ -0,0 +1,314 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+// change comments
+/*
+12/07/21 added  back code to drive to goal, changed bucket dump setting,
+         added SCORING_POSITION state to initialization. use dpad to set level
+*/
+package org.firstinspires.ftc.teamcode.OpModes;
+
+//
+// import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.ReadWriteFile;
+import com.vuforia.State;
+
+import org.firstinspires.ftc.teamcode.HardwareProfile.HardwareProfile;
+import org.firstinspires.ftc.teamcode.Libs.ArmControlCLass;
+import org.firstinspires.ftc.teamcode.Libs.DriveClass;
+import org.firstinspires.ftc.teamcode.Threads.MechControlLibrary;
+
+
+@Autonomous(name="AutoParkDriveClassJG2", group="Competition")
+//@Disabled
+public class AutoParkDriveClassJG2 extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    HardwareProfile robot   = new HardwareProfile();
+    private LinearOpMode opMode = this;
+    private ElapsedTime   runtime = new ElapsedTime();
+    private State setupState = State.ALLIANCE_SELECT;
+
+    @Override
+    public void runOpMode() {
+        // arm control thread
+      //  MechControlLibrary armControl = new MechControlLibrary(robot, robot.ARM_THREAD_SLEEP);
+     //   Thread armController = new Thread(mechControl);
+
+        boolean autoReady = false;
+        double startTime;
+        String alliance = "";
+        String startPosition = "";
+        String goalPosition = "";
+        long startDelay = 0;
+        double timeElapsed;
+        double positionFactor = 1;
+        int scorePosition=1;
+        double forwardSpeed = -0.4;
+        double goalDistance = 20.0;
+        double turnDistance = 8.0;
+        double turnAngle = 60;
+        double parkDistance = 26.0;
+        double parkAdjust = 24.0;
+        double turnError = 0.5;
+        double bucketAngle = -1.0;
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        DriveClass drive = new DriveClass(robot, opMode);
+        // arm control
+        ArmControlCLass armControl = new ArmControlCLass(robot, robot.ARM_THREAD_SLEEP);
+
+        timeElapsed = runtime.time();   // initialize timeElapsed to confirm button press time
+        while (!autoReady){
+
+            switch (setupState) {
+                case ALLIANCE_SELECT:
+                    telemetry.addData("Which Alliance are you on?", "");
+                    telemetry.addData("Press X  == ", " BLUE Alliance");
+                    telemetry.addData("Press Y  == ", " RED Alliance");
+                    telemetry.update();
+
+                    if(gamepad1.x || gamepad1.y){
+                        if (gamepad1.x){
+                            alliance = "BLUE";
+                            positionFactor = -1;
+ //                          robot.LEDPort.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+                        }  else {
+                            alliance = "RED";
+                            positionFactor = 1;
+ //                           robot.LEDPort.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+                        }
+                        setupState = State.DELAY_LENGTH;    // give option to add delay
+                        telemetry.addData("Goal on ==  ", positionFactor);
+                        telemetry.update();
+                    }   // end of if(gamepad1.x...
+                    break;
+
+                case DELAY_LENGTH:
+                    telemetry.addData("Add a Delay", "");
+                    telemetry.addData("Press DPAD_UP  ", " To Increase Delay");
+                    telemetry.addData("Press DPAD_DOWN ", " To Reduce Delay");
+                    telemetry.addData("Current Delay = ", startDelay);
+
+                    telemetry.addData("Press A to continue", "");
+                    telemetry.update();
+
+                    if(gamepad1.dpad_up && (runtime.time()-timeElapsed) >0.3) {
+                        if(startDelay < 10){       // limit the max delay to 10 seconds
+                            startDelay = startDelay + 1;
+                        }
+                        timeElapsed = runtime.time();
+                    } //end of if(gamepad1.dpad_up)
+
+                    if(gamepad1.dpad_down && (runtime.time()-timeElapsed) >0.3) {
+                        if(startDelay > 0){       // confirm that the delay is not negative
+                            startDelay = startDelay - 1;
+                        } else startDelay = 0;
+                        timeElapsed = runtime.time();
+                    } //end of if(gamepad1.dpad_up)
+
+                    if(gamepad1.a){         // exit the setup
+                        setupState = State.POSITION_SELECT;
+                        startDelay = startDelay * 1000;
+                    }   // end of if(gamepad1.x...
+                    break;
+
+                case POSITION_SELECT:
+                    telemetry.addData("What is the position of the robot?", "");
+                    telemetry.addData("Press DPAD Left  == ", " WALL Side");
+                    telemetry.addData("Press DPAD Right == ", " FIELD Side");
+                    telemetry.update();
+
+                    if(gamepad1.dpad_left || gamepad1.dpad_right){
+                        if (gamepad1.dpad_left){
+                            startPosition = "WALL";
+                            sleep(1000);
+                        }  else {
+                            startPosition = "FIELD";
+                            positionFactor = positionFactor * -1;
+                        }
+                        setupState = State.SCORING_POSITION;
+                    }   // end of if(gamepad1.dpad_left...
+                    break;
+
+                case SCORING_POSITION:
+                    telemetry.addData("Set Scoring Position", "");
+                    telemetry.addData("Press DPAD_UP  ", " To Increase ");
+                    telemetry.addData("Press DPAD_DOWN ", " To Decrease");
+                    telemetry.addData("Current Scoring Position = ", scorePosition);
+
+                    telemetry.addData("Press A to continue", "");
+                    telemetry.update();
+
+                    if(gamepad1.dpad_up && (runtime.time()-timeElapsed) >0.3) {
+                        if(scorePosition < 3){       // limit the max delay to 10 seconds
+                            scorePosition = scorePosition + 1;
+                        }
+                        timeElapsed = runtime.time();
+                    } //end of if(gamepad1.dpad_up)
+
+                    if(gamepad1.dpad_down && (runtime.time()-timeElapsed) >0.3) {
+                        if(scorePosition > 1){       // confirm that the delay is not negative
+                            scorePosition = scorePosition - 1;
+                        } else scorePosition = 1;
+                        timeElapsed = runtime.time();
+                    } //end of if(gamepad1.dpad_up)
+
+                    if(gamepad1.a){         // exit the setup
+                        setupState = State.VERIFY_CONFIG;
+                    }   // end of Scoring_position...
+                    break;
+
+                case VERIFY_CONFIG:
+                    if (positionFactor==1) {
+                        goalPosition="Right";
+                    }else{
+                        goalPosition="Left";
+                    }
+                    telemetry.addData("Verify the setup", "");
+                    telemetry.addData("Alliance          == ", alliance);
+                    telemetry.addData("Start Delay == ", startDelay);
+                    telemetry.addData("Starting Position ==  ", startPosition);
+                    telemetry.addData("Goal position ==  ", goalPosition);
+                    telemetry.addData("Goal on ==  ", positionFactor);
+                    telemetry.addData("Scoring Position ==  ", scorePosition);
+                    telemetry.addData("","");
+                    telemetry.addData("Press A to Confirm or B to start over","");
+                    telemetry.update();
+
+                    if(gamepad1.b || gamepad1.a){
+                        if (gamepad1.b){
+                            autoReady = false;
+                            startDelay = 0;     // reset the start delay
+                            setupState = State.ALLIANCE_SELECT;
+                        }  else {
+                            autoReady = true;
+                        }
+                    }   // end of if(gamepad1.right_bumper...
+                    break;
+            }   // end of switch(setupState)
+        }   // end of while(autoReady)
+
+        
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        robot.intakeDeployBlue.setPosition(robot.BLUE_ZERO);
+        robot.intakeDeployPink.setPosition(robot.PINK_ZERO);
+
+        waitForStart();
+        startTime = runtime.time() + startDelay;
+
+        // start arm controller thread
+    //    armController.start();
+
+        // sleep for set delay time
+        sleep(startDelay);
+
+        while (opModeIsActive()) {
+
+            // Step 1
+            //      drive.driveStraight(backwards to goal
+            drive.driveStraight(forwardSpeed, goalDistance);
+
+            //   drive.driveTurn(turn towards goal);
+            drive.driveTurn(turnAngle * positionFactor, turnError);
+
+            //move arm to scoring positions
+            if(scorePosition==1){
+                armControl.scoringPos1();
+            }else if(scorePosition==2){
+                armControl.scoringPos2();
+            }else if(scorePosition==3){
+                armControl.scoringPos3();
+            }
+        //    idle();
+            //  drive towards goal
+            drive.driveStraight(forwardSpeed, turnDistance);
+
+            // score element in high goal
+            //move arm to scoring positions. initailly always go high, later will use
+            // recognition to set scorePosition
+            sleep(1000);
+
+            // dump bucket
+            robot.bucketDump.setPosition(bucketAngle);
+            sleep(1000);
+            //reset arms
+            robot.bucketDump.setPosition(0);
+
+            armControl.moveToZero();
+
+            // reverse direction to drive forward to park
+            forwardSpeed = forwardSpeed * -1;
+
+            //back up turn amount
+            drive.driveStraight(forwardSpeed, turnDistance);
+
+            // reposition angle to park
+            turnAngle = turnAngle * -1;
+            drive.driveTurn(turnAngle * positionFactor, turnError);
+
+            // adjust park distance if needed
+            //if (startPosition.equals("FIELD")) {
+            //    parkDistance = parkDistance + parkAdjust;
+            //}
+            drive.driveStraight(forwardSpeed, goalDistance);
+
+            // Step 2 - just stop for now
+        }
+        drive.motorsHalt();
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
+
+        //stops mechanism thread
+        //mechControl.stop();
+
+    } // end of opmode
+
+    /*
+     * Enumerate the states of the machine
+     */
+    enum State {
+        ALLIANCE_SELECT, POSITION_SELECT, VERIFY_CONFIG, DELAY_LENGTH,SCORING_POSITION,
+        FIELD_PARK, HALT
+    }   // end of enum State
+}
